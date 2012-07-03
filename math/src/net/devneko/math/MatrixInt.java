7b9e017bad8694bf0f41954744f2b88d80aa0875
shinji <dotneet@gmail.com>
2012-07-03 13:09:14 +0900

    add MatrixInt#intersect()、MatrixInt#unit()

--- math/src/net/devneko/math/MatrixInt.java
+++ math/src/net/devneko/math/MatrixInt.java
@@ -15,7 +15,7 @@ public class MatrixInt {
 	public void setValue(int x, int y, int value) {
 		mat[y][x] = value;
 	}
-	public int getWidht() {
+	public int getWidth() {
 		return this.width;
 	}
 	public int getHeight() {
@@ -30,5 +30,39 @@ public class MatrixInt {
 			}
 		}
 		return m;
+	}
+	
+	/**
+	 * 単位行列を作成する.
+	 * @param size
+	 */
+	public static MatrixInt unit(int size) {
+		MatrixInt mat = new MatrixInt(size, size);
+		for ( int i = 0; i < size; i++ ) {
+			mat.setValue(i,i,1);
+		}
+		return mat;
+	}
+	
+	/**
+	 * 行列の積を計算する.
+	 * @param m
+	 * @return
+	 */
+	public MatrixInt intersect(MatrixInt m) {
+		if ( getWidth() != m.getHeight() || getHeight() != m.getWidth() ) {
+			throw new IllegalArgumentException("matrix size must be same.");
+		}
+		MatrixInt newMat = new MatrixInt(height, m.width);
+		for ( int x = 0; x < m.width; x++ ) {
+			for ( int y = 0; y < height; y++ ) {
+				int sum = 0;
+				for ( int i = 0; i < height; i++ ) {
+					sum += getValue(x,i) * m.getValue(i,y);
+				}
+				newMat.setValue(x, y, sum);
+			}
+		}
+		return newMat;
 	}
 }
